c3f8015e91432fade7628f52a57d6acb504291f0
tanhLinhphong <142803556+tanhLinhphong@users.noreply.github.com>
2023-08-25 10:51:23 +0700

    Add files via upload

---
+++ bai1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a,b,sum;
+	printf("Enter the first munber:");
+	scanf("%d",&a);
+	printf("Enter the second number:");
+	scanf("%d",&b);
+	sum = a+b;
+	printf("The sum is :%d",sum);
+	return 0;
+}
