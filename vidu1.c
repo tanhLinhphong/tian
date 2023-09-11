7313e3573eff0e4828309f1a41d1601e1f6c0d68
tanhLinhphong <142803556+tanhLinhphong@users.noreply.github.com>
2023-09-11 10:40:18 +0700

    Add files via upload

---
+++ vidu1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int count;
+	printf("\t This is a \n");
+	for (count = 1 ; count <=3; count++)
+	printf("\n\t\t nice");
+	printf("\n\t\t world.\n");
+	return 0;
+}
+
