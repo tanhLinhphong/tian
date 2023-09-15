0e1256b8f912d7bea618cab36a299cf73cb12465
tanhLinhphong <142803556+tanhLinhphong@users.noreply.github.com>
2023-09-15 09:33:44 +0700

    bai10

--- vidu1.c
+++ vidu1.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 int main()
 {
-	int count;
-	printf("\t This is a \n");
-	for (count = 1 ; count <=3; count++)
-	printf("\n\t\t nice");
-	printf("\n\t\t world.\n");
+	int num;
+	printf("\The even Numbers from 1 to 30 are \n");
+	for (num=2;num<300;num+=2)
+	printf("%d\n",num);
 	return 0;
 }
-
