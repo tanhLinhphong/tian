86b43e502725c853742ec9cd1896d3498b56bf64
tanhLinhphong <142803556+tanhLinhphong@users.noreply.github.com>
2023-09-18 10:38:55 +0700

    Add files via upload

---
+++ menu.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+ void menuDoUong();
+ void menuDoAn();
+  int main()
+ {
+ 	printf("menu do uong\n");
+ 	menuDoUong();
+ 	printf("menu do an\n");
+ 	menuDoAn();
+ 	return 0 ;
+ }
+ void menuDoUong(){
+ 
+ printf("1.TRA DAO\n");
+ printf("2.Cam xa\n");
+ printf("3.Tra chanh\n");
+}
+void menuDoAn(){
+	printf("1.GA Luoc\n");
+	printf("2.Pho\n");
+    printf("3.Ga Ran KFC\n");
+}
